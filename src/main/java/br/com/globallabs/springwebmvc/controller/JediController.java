5427c790f1001e68f17bc06c48a4f9edde0e4a03
gisesonia <gisesonia@gmail.com>
2020-12-19 09:26:21 -0300

    Atualizando funcionalidades

--- src/main/java/br/com/globallabs/springwebmvc/controller/JediController.java
+++ src/main/java/br/com/globallabs/springwebmvc/controller/JediController.java
@@ -4,35 +4,45 @@ import br.com.globallabs.springwebmvc.model.Jedi;
 import br.com.globallabs.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;//Versões posteriores a 8, tem List.of()
+import java.util.Optional;
 import javax.validation.Valid;//dependencia adicionado no Gradle - implementation 'org.springframework.boot:spring-boot-starter-validation'
 
 @Controller
 public class JediController {
 
-    //Injeção de dependência. Está criando uma instância da classe repository. Acessa o método getAllJedi()
     @Autowired
     private JediRepository repository;
 
     @GetMapping("/jedi")
     public ModelAndView jedi(){
+
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("jedi");
 
-        //final Jedi luke = new Jedi("Luke","Skywalker"); Testando adicionando direto
-        //modelAndView.addObject("allJedi", List.of(luke));
 
-        modelAndView.addObject("allJedi", repository.getAllJedi());
+        modelAndView.addObject("allJedi", repository.findAll());
 
         return modelAndView;
     }
+
+    @GetMapping("/search")
+    public ModelAndView search(@RequestParam(value = "name") final String name) {
+
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+
+        modelAndView.addObject("allJedi", repository.findByNameContainingIgnoreCase(name));
+
+        return modelAndView;
+    }
+
 
     @GetMapping("/new-jedi")
     public ModelAndView newJedi(){
@@ -43,7 +53,7 @@ public class JediController {
         modelAndView.addObject("jedi", new Jedi());
         return modelAndView;
     }
-    //Post
+
     @PostMapping("/jedi")
     public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirectAttributes){
 
@@ -51,9 +61,34 @@ public class JediController {
             return "new-jedi";
         }
 
-        repository.add(jedi);
+        repository.save(jedi);
 
-        redirectAttributes.addFlashAttribute("message","Jedi cadastrado com sucesso.");
+        redirectAttributes.addFlashAttribute("message", "Jedi cadatrado com sucesso.");
+
         return "redirect:jedi";
+
     }
+
+    @GetMapping("/jedi/{id}/delete")
+    public String deleteJedi(@PathVariable("id") final Long id, RedirectAttributes redirectAttributes) {
+
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        repository.delete(jedi.get());
+
+        redirectAttributes.addFlashAttribute("message", "Jedi removido com sucesso.");
+
+        return "redirect:/jedi" ;
+    }
+
+    @GetMapping("/jedi/{id}/update")
+    public String updateJedi(@PathVariable("id") final Long id, Model model) {
+
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        model.addAttribute("jedi", jedi.get());
+
+        return "edit-jedi";
+    }
+
 }

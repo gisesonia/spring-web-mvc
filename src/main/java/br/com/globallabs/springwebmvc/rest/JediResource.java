b373c57b8786d5119c5eca34aa010b48b6d2c9db
gisesonia <gisesonia@gmail.com>
2020-12-19 20:10:05 -0300

    Update e refatoração

--- src/main/java/br/com/globallabs/springwebmvc/rest/JediResource.java
+++ src/main/java/br/com/globallabs/springwebmvc/rest/JediResource.java
@@ -3,6 +3,7 @@ package br.com.globallabs.springwebmvc.rest;
 import br.com.globallabs.springwebmvc.exception.JediNotFoundException;
 import br.com.globallabs.springwebmvc.model.Jedi;
 import br.com.globallabs.springwebmvc.repository.JediRepository;
+import br.com.globallabs.springwebmvc.service.JediService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,13 @@ import java.util.Optional;
 
 @RestController
 public class JediResource {
-    @Autowired
-    private JediRepository repository;
+
+    @Autowired      //Substitui a classe repository pela classe service que importa o repository
+    private JediService service;
 
     @GetMapping("/api/jedi")
     public List<Jedi> getAllJedi() {
-        return repository.findAll();
+        return service.findAll();
     }
 
     /* Rota por id
@@ -35,48 +37,28 @@ public class JediResource {
       }*/
     @GetMapping("/api/jedi/{id}")
     public ResponseEntity<Jedi> getJedi(@PathVariable("id") Long id) {//ResponseEntity do Spring
-        final Optional<Jedi> jedi = repository.findById(id);
+        final Jedi jedi = service.findById(id);
 
-        if (jedi.isPresent()) {
-            return ResponseEntity.ok(jedi.get());//http status 200
-        } else {
-            return ResponseEntity.notFound().build();// status code 404
-        }
+        return ResponseEntity.ok(jedi);//http status 200
     }
 
     @PostMapping("/api/jedi")
     @ResponseStatus(HttpStatus.CREATED)
     public Jedi createJedi(@Valid @RequestBody Jedi jedi) {//Aqui é do tipo json por isso @RequestBody
-        return repository.save(jedi);
+        return service.save(jedi);
     }
 
     @PutMapping("/api/jedi/{id}")
     public ResponseEntity<Jedi> updateJedi(@PathVariable("id") Long id, @Valid @RequestBody Jedi dto){//dto é data transfer object, o objeto recebido json
 
-        final Optional<Jedi> jediEntity = repository.findById(id);
-        final Jedi jedi;
+        final Jedi jedi = service.update(id, dto);
 
-        if(jediEntity.isPresent()){
-            jedi = jediEntity.get();
-        }else {
-            return ResponseEntity.notFound().build();
-        }
-        //jedi é do banco de dados e dto é do json e são inseridos no banco com o set
-        jedi.setName(dto.getName());
-        jedi.setLastName(dto.getLastName());
-
-        return ResponseEntity.ok(repository.save(jedi));
+        return ResponseEntity.ok(jedi);
     }
 
     @DeleteMapping("/api/jedi/{id}")
-    public ResponseEntity delete(@PathVariable("id") Long id) {
-       final Optional<Jedi> jedi = repository.findById(id);
-
-       if(jedi.isPresent()){
-           repository.delete(jedi.get());
-           return ResponseEntity.noContent().build(); //404
-       } else {
-           return  ResponseEntity.notFound().build();
-       }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        service.delete(id);
     }
 }
